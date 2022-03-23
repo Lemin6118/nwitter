f317bf50c18be7c12ce9ad0ab29d10863405fd1d
Lee MinSeok <Lemin6118@email.daelim.ac.kr>
2022-03-23 20:32:53 +0900

    1.0 Initialization

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA0FvK9yTq76hMTZGbv36POXYmxaHiZKss",
-  authDomain: "nwitter-7b61b.firebaseapp.com",
-  projectId: "nwitter-7b61b",
-  storageBucket: "nwitter-7b61b.appspot.com",
-  messagingSenderId: "541656071904",
-  appId: "1:541656071904:web:d4ef05a78c120b85889a85"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID
 };
 
 // Initialize Firebase
